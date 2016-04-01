bc0ffae781342653d7b42b5207203a205384c103
Aleksandr Zakharov <human.of.rain@gmail.com>
2016-04-01 07:41:18 +0300

    rspec spec/crud_spec.rb -e rq02

--- app/models/Racer.rb
+++ app/models/Racer.rb
@@ -1,5 +1,7 @@
 class Racer
 
+	attr_accessor :id, :number, :first_name, :last_name, :gender, :group, :secs
+
 	def self.mongo_client
 		Mongoid::Clients.default
 	end
@@ -7,5 +9,10 @@ class Racer
 	def self.collection
 		self.mongo_client[:racers]
 	end
-	
+
+	def self.all(prototype={},sort={:number => 1},skip=0,limit=nil)
+		result = collection.find(prototype).sort(sort).skip(skip)
+		result = result.limit(limit) if !limit.nil?
+		return result
+	end
 end
